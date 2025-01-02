bf160fdee99d360c7f93f0a1eb119ded563fa9f4
Bjørnar Berge <bjornar.berge.07@gmail.com>
2025-01-02 15:02:51 +0100

    changes

--- src/pages/Dashboard/WelcomePage.jsx
+++ src/pages/Dashboard/WelcomePage.jsx
@@ -64,13 +64,14 @@ export default function WelcomePage(props) {
           />
         </svg>
         </CardDataStats></a>
+
         <a href='/chat' className='dark:bg-black'><CardDataStats title="" total="Meldinger" rate="">
           <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M7 9H17M7 13H17M21 20L17.6757 18.3378C17.4237 18.2118 17.2977 18.1488 17.1656 18.1044C17.0484 18.065 16.9277 18.0365 16.8052 18.0193C16.6672 18 16.5263 18 16.2446 18H6.2C5.07989 18 4.51984 18 4.09202 17.782C3.71569 17.5903 3.40973 17.2843 3.21799 16.908C3 16.4802 3 15.9201 3 14.8V7.2C3 6.07989 3 5.51984 3.21799 5.09202C3.40973 4.71569 3.71569 4.40973 4.09202 4.21799C4.51984 4 5.0799 4 6.2 4H17.8C18.9201 4 19.4802 4 19.908 4.21799C20.2843 4.40973 20.5903 4.71569 20.782 5.09202C21 5.51984 21 6.0799 21 7.2V20Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
         </CardDataStats></a>
-        <button className='dark:bg-black' onClick={handleLogout} id='rightEl'><CardDataStats title="" total="Logg ut" rate="">
-          
+
+        <button className='dark:bg-black' onClick={handleLogout} id='rightEl'><CardDataStats title="" total="Logg ut" rate="">  
         <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M15 12L2 12M2 12L5.5 9M2 12L5.5 15" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
           <path d="M9.00195 7C9.01406 4.82497 9.11051 3.64706 9.87889 2.87868C10.7576 2 12.1718 2 15.0002 2L16.0002 2C18.8286 2 20.2429 2 21.1215 2.87868C22.0002 3.75736 22.0002 5.17157 22.0002 8L22.0002 16C22.0002 18.8284 22.0002 20.2426 21.1215 21.1213C20.3531 21.8897 19.1752 21.9862 17 21.9983M9.00195 17C9.01406 19.175 9.11051 20.3529 9.87889 21.1213C10.5202 21.7626 11.4467 21.9359 13 21.9827" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round"/>
